63417445ef704947809fd88d1d4a39c256b219ca
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2018-12-29 01:52:41 -0600

    * Added detail directory to hold distracting preprocessor/macro/detail code that shouldn't distract from the public interface * Added constexpr as appropriate to C++17 header. C++11/C++14 will follow.

---
+++ include/boost/math/detail/compiler_config.hpp
@@ -0,0 +1,21 @@
+//               Copyright Kedar R. Bhat 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//      (See accompanying file LICENSE_1_0.txt or copy at
+//           https://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef AUTODIFF_COMPILER_CONFIG_HPP
+#define AUTODIFF_COMPILER_CONFIG_HPP
+
+#include <boost/config.hpp>
+#ifdef BOOST_COMPILER_CONFIG
+#include BOOST_COMPILER_CONFIG
+#endif
+
+#ifdef _MSVC_LANG
+#define BOOST_MATH_AUTODIFF_CPP_STD _MSVC_LANG
+#else
+#define BOOST_MATH_AUTODIFF_CPP_STD __cplusplus
+#endif
+
+#endif //AUTODIFF_COMPILER_CONFIG_HPP

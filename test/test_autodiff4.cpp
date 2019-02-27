67e382ac976665b5d8a7535b968bc1507fdc99de
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2019-02-27 17:16:47 -0600

    removed preprocessor code in favor for if constexpr

--- test/test_autodiff4.cpp
+++ test/test_autodiff4.cpp
@@ -5,8 +5,6 @@
 
 #include "test_autodiff.hpp"
 
-#include <boost/mp11/function.hpp>
-
 BOOST_AUTO_TEST_SUITE(test_autodiff_4)
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(lround_llround_lltrunc_truncl, T, all_float_types) {
@@ -31,7 +29,7 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(lround_llround_lltrunc_truncl, T, all_float_types)
 #endif
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(multiprecision, T, all_float_types) {
+BOOST_AUTO_TEST_CASE_TEMPLATE(multiprecision, T, multiprecision_float_types) {
   const T eps = 30 * std::numeric_limits<T>::epsilon();
   constexpr int Nw = 3;
   constexpr int Nx = 2;

80e1dbc9e7f36c3e3c7a23beae97cea9dd60fa8f
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2019-03-10 03:50:19 -0500

    added back promote_args

--- include/boost/math/differentiation/autodiff.hpp
+++ include/boost/math/differentiation/autodiff.hpp
@@ -1716,6 +1716,11 @@ struct promote_args_2<RealType0, differentiation::detail::fvar<RealType1, Order1
   using type = differentiation::detail::fvar<typename promote_args_2<RealType0, RealType1>::type, Order1>;
 };
 
+template <typename RealType, std::size_t Order>
+struct promote_args<differentiation::detail::fvar<RealType, Order>> {
+  using type = differentiation::detail::fvar<typename promote_args<RealType>::type, Order>;
+};
+
 template <typename destination_t, typename RealType, std::size_t Order>
 inline destination_t real_cast(const differentiation::detail::fvar<RealType, Order>& from_v) {
   using root_type = differentiation::detail::get_root_type<decltype(from_v)>;

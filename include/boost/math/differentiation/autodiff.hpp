1d1d1011a9b8d571f7f8f53ae8797ba97adbc240
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2019-03-15 20:17:36 -0500

    try more specific exception specification for move constructor/assignment operator

--- include/boost/math/differentiation/autodiff.hpp
+++ include/boost/math/differentiation/autodiff.hpp
@@ -17,7 +17,7 @@
 #include <boost/mp11/algorithm.hpp>
 #include <boost/mp11/function.hpp>
 #include <boost/mp11/integral.hpp>
-#include <boost/type_traits/decay.hpp>
+#include <boost/type_traits.hpp>
 
 #include <algorithm>
 #include <array>
@@ -140,7 +140,7 @@ class fvar {
   fvar(const fvar &) = default;
 
   // RealType(cr) | RealType | RealType is move constructible.
-  fvar(fvar&&) = default;
+  fvar(fvar&&) noexcept(boost::is_nothrow_move_constructible<RealType>::value) = default;
 
   // Be aware of implicit casting from one fvar<> type to another by this copy
   // constructor.
@@ -162,7 +162,7 @@ class fvar {
   fvar &operator=(const fvar &) = default;
 
   // r = cr | RealType& | Move assignment operator.
-  fvar &operator=(fvar &&) = default;
+  fvar &operator=(fvar &&) noexcept(boost::is_nothrow_move_assignable<RealType>::value) = default;
 
   // r = ca | RealType& | Assignment operator from the arithmetic types.
   // Handled by constructor that takes a single parameter of generic type.

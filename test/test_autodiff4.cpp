2f732819823e187dc2e21bd096daa4b2ce148ff0
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2019-03-17 15:09:52 -0500

    use newer test suite

--- test/test_autodiff4.cpp
+++ test/test_autodiff4.cpp
@@ -4,21 +4,22 @@
 //           https://www.boost.org/LICENSE_1_0.txt)
 
 #include "test_autodiff.hpp"
+#include <boost/test/tools/floating_point_comparison.hpp>
 
 BOOST_AUTO_TEST_SUITE(test_autodiff_4)
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(lround_llround_lltrunc_truncl, T, all_float_types) {
+  using boost::math::lltrunc;
   using std::llround;
   using std::lround;
   using std::truncl;
-  using boost::math::lltrunc;
 
-  constexpr int m = 3;
-  const T cx = 3.25;
+  constexpr std::size_t m = 3;
+  const T& cx = static_cast<T>(3.25);
   auto x = make_fvar<T, m>(cx);
-  long yl = lround(x);
+  auto yl = lround(x);
   BOOST_REQUIRE_EQUAL(yl, lround(cx));
-  long long yll = llround(x);
+  auto yll = llround(x);
   BOOST_REQUIRE_EQUAL(yll, llround(cx));
   BOOST_REQUIRE_EQUAL(lltrunc(cx), lltrunc(x));
 
@@ -29,12 +30,30 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(lround_llround_lltrunc_truncl, T, all_float_types)
 #endif
 }
 
+BOOST_AUTO_TEST_CASE_TEMPLATE(equality, T, all_float_types) {
+  BOOST_MATH_STD_USING
+  using boost::math::fpclassify;
+  using std::fpclassify;
+  using boost::math::ulp;
+  using boost::math::epsilon_difference;
+
+  constexpr std::size_t m = 3;
+  // check zeros
+  {
+    auto x = make_fvar<T, m>(0.0);
+    auto y = T(-0.0);
+    BOOST_REQUIRE_EQUAL(x, y);
+  }
+}
+
 BOOST_AUTO_TEST_CASE_TEMPLATE(multiprecision, T, multiprecision_float_types) {
+  BOOST_MATH_STD_USING
+
   const T eps = 30 * std::numeric_limits<T>::epsilon();
-  constexpr int Nw = 3;
-  constexpr int Nx = 2;
-  constexpr int Ny = 4;
-  constexpr int Nz = 3;
+  constexpr std::size_t Nw = 3;
+  constexpr std::size_t Nx = 2;
+  constexpr std::size_t Ny = 4;
+  constexpr std::size_t Nz = 3;
   const auto w = make_fvar<T, Nw>(11);
   const auto x = make_fvar<T, 0, Nx>(12);
   const auto y = make_fvar<T, 0, 0, Ny>(13);
@@ -45,33 +64,8 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(multiprecision, T, multiprecision_float_types) {
       "1976.3196007477977177798818752904187209081211892187"
       "5499076582535951111845769110560421820940516423255314");
   // BOOST_REQUIRE_CLOSE(v.derivative(Nw,Nx,Ny,Nz), answer, eps); // Doesn't work for cpp_dec_float
-  using std::fabs;
   const T relative_error = static_cast<T>(fabs(v.derivative(Nw, Nx, Ny, Nz) / answer - 1));
   BOOST_REQUIRE_LT(relative_error, eps);
-}
-
-BOOST_AUTO_TEST_CASE_TEMPLATE(autodiff_tanh, T, all_float_types) {
-  using std::tanh;
-  using boost::multiprecision::tanh;
-  using boost::math::differentiation::detail::tanh;
-
-  using test_constants = test_constants_t<T>;
-  static constexpr auto m = test_constants::order;
-  test_detail::RandomSample<T> x_sampler{-10, 10};
-  for (auto i : boost::irange(test_constants::n_samples)) {
-    std::ignore = i;
-    auto x = x_sampler.next();
-    try {
-      BOOST_REQUIRE_CLOSE(tanh(make_fvar<T, m>(x)), tanh(x),
-                          test_constants::pct_epsilon());
-    } catch (const boost::math::rounding_error &) {
-      BOOST_REQUIRE_THROW(tanh(make_fvar<T, m>(x)), boost::wrapexcept<boost::math::rounding_error>);
-      BOOST_REQUIRE_THROW(tanh(x), boost::wrapexcept<boost::math::rounding_error>);
-    } catch (...) {
-      std::cout << "Input: x: " << x << std::endl;
-      std::rethrow_exception(std::exception_ptr(std::current_exception()));
-    }
-  }
 }
 
 BOOST_AUTO_TEST_SUITE_END()

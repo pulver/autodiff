3f082b070dccd58c1bfb2f9a4d01e6beaf872e3a
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2019-03-22 15:14:14 -0500

    move a large chunch of test_autodiff5 to test_autodiff4 due to Cygwin still choking on too many sections issues

--- test/test_autodiff4.cpp
+++ test/test_autodiff4.cpp
@@ -6,6 +6,8 @@
 #include "test_autodiff.hpp"
 #include <boost/test/tools/floating_point_comparison.hpp>
 
+using namespace boost::math::differentiation;
+
 BOOST_AUTO_TEST_SUITE(test_autodiff_4)
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(lround_llround_lltrunc_truncl, T, all_float_types) {
@@ -67,5 +69,165 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(multiprecision, T, multiprecision_float_types) {
   const T relative_error = static_cast<T>(fabs(v.derivative(Nw, Nx, Ny, Nz) / answer - 1));
   BOOST_REQUIRE_LT(relative_error, eps);
 }
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(airy_hpp, T, all_float_types) {
+
+  using boost::multiprecision::min;
+  using std::min;
+
+  using test_constants = test_constants_t<T>;
+  static constexpr auto m = test_constants::order;
+
+  test_detail::RandomSample<T> x_sampler(-100, 100);
+  for (auto i : boost::irange(test_constants::n_samples)) {
+    const auto& x = x_sampler.next();
+    {
+      auto autodiff_v = boost::math::airy_ai(make_fvar<T, m>(x));
+      auto anchor_v = boost::math::airy_ai(x);
+      BOOST_REQUIRE_CLOSE(autodiff_v, anchor_v,
+                          1e4 * test_constants::pct_epsilon());
+    }
+
+    {
+      auto autodiff_v = boost::math::airy_ai_prime(make_fvar<T, m>(x));
+      auto anchor_v = boost::math::airy_ai_prime(x);
+      BOOST_REQUIRE_CLOSE(autodiff_v, anchor_v,
+                          1e4 * test_constants::pct_epsilon());
+    }
+
+    {
+      auto x_ = (min)(x, T(26));
+      auto autodiff_v = boost::math::airy_bi(make_fvar<T, m>(x_));
+      auto anchor_v = boost::math::airy_bi(x_);
+      BOOST_REQUIRE_CLOSE(autodiff_v, anchor_v,
+                          1e4 * test_constants::pct_epsilon());
+    }
+
+    {
+      auto x_ = ((min))(x, T(26));
+      auto autodiff_v = boost::math::airy_bi_prime(make_fvar<T, m>(x_));
+      auto anchor_v = boost::math::airy_bi_prime(x_);
+      BOOST_REQUIRE_CLOSE(autodiff_v, anchor_v,
+                          1e4 * test_constants::pct_epsilon());
+    }
+
+    if (i > 0) {
+      {
+        auto autodiff_v = boost::math::airy_ai_zero<autodiff_fvar<T, m>>(i);
+        auto anchor_v = boost::math::airy_ai_zero<T>(i);
+        BOOST_REQUIRE_CLOSE(autodiff_v, anchor_v,
+                            1e4 * test_constants::pct_epsilon());
+      }
+
+      {
+        auto autodiff_v = boost::math::airy_bi_zero<autodiff_fvar<T, m>>(i);
+        auto anchor_v = boost::math::airy_bi_zero<T>(i);
+        BOOST_REQUIRE_CLOSE(autodiff_v, anchor_v,
+                            1e4 * test_constants::pct_epsilon());
+      }
+    }
+  }
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(acosh_hpp, T, all_float_types) {
+  using boost::math::acosh;
+  using test_constants = test_constants_t<T>;
+  static constexpr auto m = test_constants::order;
+
+  test_detail::RandomSample<T> x_sampler{1, 100};
+  for (auto i : boost::irange(test_constants::n_samples)) {
+    std::ignore = i;
+    auto x = x_sampler.next();
+    auto autodiff_v = acosh(make_fvar<T, m>(x));
+    auto anchor_v = acosh(x);
+    BOOST_REQUIRE_CLOSE(autodiff_v, anchor_v,
+                        1e3 * test_constants::pct_epsilon());
+  }
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(asinh_hpp, T, all_float_types) {
+  using boost::math::asinh;
+  using test_constants = test_constants_t<T>;
+  static constexpr auto m = test_constants::order;
+
+  test_detail::RandomSample<T> x_sampler{-100, 100};
+  for (auto i : boost::irange(test_constants::n_samples)) {
+    std::ignore = i;
+    auto x = x_sampler.next();
+
+    auto autodiff_v = asinh(make_fvar<T, m>(x));
+    auto anchor_v = asinh(x);
+    BOOST_REQUIRE_CLOSE(autodiff_v, anchor_v,
+                        1e3 * test_constants::pct_epsilon());
+  }
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(atanh_hpp, T, all_float_types) {
+  using boost::math::nextafter;
+  using std::nextafter;
+
+  using boost::math::atanh;
+  using test_constants = test_constants_t<T>;
+  static constexpr auto m = test_constants::order;
+
+  test_detail::RandomSample<T> x_sampler{-1, 1};
+  for (auto i : boost::irange(test_constants::n_samples)) {
+    std::ignore = i;
+    auto x = nextafter(x_sampler.next(), T(0));
+
+    auto autodiff_v = atanh(make_fvar<T, m>(x));
+    auto anchor_v = atanh(x);
+    BOOST_REQUIRE_CLOSE(autodiff_v, anchor_v,
+                        1e3 * test_constants::pct_epsilon());
+  }
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(atan_hpp, T, all_float_types) {
+  using std::atan;
+  using boost::multiprecision::atan;
+  using boost::math::differentiation::detail::atan;
+  using boost::math::signbit;
+  using boost::multiprecision::signbit;
+  using boost::math::fpclassify;
+  using boost::multiprecision::fpclassify;
+  using boost::math::float_prior;
+
+  using test_constants = test_constants_t<T>;
+  static constexpr auto m = test_constants::order;
+
+  test_detail::RandomSample<T> x_sampler{-1, 1};
+  for (auto i : boost::irange(test_constants::n_samples)) {
+    std::ignore = i;
+    auto x = T(1);
+    while(fpclassify(T(abs(x)-1)) == FP_ZERO) {
+      x = x_sampler.next();
+    }
+
+    auto autodiff_v = atan(make_fvar<T, m>(x));
+    auto anchor_v = atan(x);
+    BOOST_REQUIRE_CLOSE(autodiff_v, anchor_v,
+                        1e3 * test_constants::pct_epsilon());
+  }
+}
+
+/*
+BOOST_AUTO_TEST_CASE_TEMPLATE(atan2_function, T, all_float_types) {
+  using test_constants = test_constants_t<T>;
+  static constexpr auto m = test_constants::order;
+
+  test_detail::RandomSample<T> x_sampler{-2000, 2000};
+  test_detail::RandomSample<T> y_sampler{-2000, 2000};
+
+  for (auto i : boost::irange(test_constants::n_samples)) {
+    std::ignore = i;
+    auto x = x_sampler.next();
+    auto y = y_sampler.next();
+
+    auto autodiff_v = atan2(make_fvar<T, m>(x), make_fvar<T, m>(y));
+    auto anchor_v = atan2(x, y);
+    BOOST_REQUIRE_CLOSE(autodiff_v, anchor_v, 200 * test_constants::pct_epsilon());
+  }
+}
+*/
 
 BOOST_AUTO_TEST_SUITE_END()

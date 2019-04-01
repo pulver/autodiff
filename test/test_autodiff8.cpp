36ca28a90ca4bd9943f4456eca491c19da08e3f3
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2019-03-31 22:28:40 -0500

    test_autodiff5.cpp: remove non-ASCII characters test_autodiff7.cpp: add copyright/license information test_autodiff8.cpp: add copyright/license information

--- test/test_autodiff8.cpp
+++ test/test_autodiff8.cpp
@@ -1,3 +1,8 @@
+//           Copyright Matthew Pulver 2018 - 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//      (See accompanying file LICENSE_1_0.txt or copy at
+//           https://www.boost.org/LICENSE_1_0.txt)
+
 #include "test_autodiff.hpp"
 
 BOOST_AUTO_TEST_SUITE(test_autodiff_8)

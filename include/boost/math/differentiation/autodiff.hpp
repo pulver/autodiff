ae1332c8f444f262421108221f070d9e12600921
Kedar R. Bhat <kedar.r.bhat@gmail.com>
2019-04-27 11:48:11 -0500

    add BOOST_MATH_CONSTEXPR to real_cast overload

--- include/boost/math/differentiation/autodiff.hpp
+++ include/boost/math/differentiation/autodiff.hpp
@@ -1933,7 +1933,7 @@ struct promote_args_2<RealType0, differentiation::detail::fvar<RealType1, Order1
 };
 
 template<typename destination_t, typename RealType, std::size_t Order>
-inline destination_t real_cast(const differentiation::detail::fvar<RealType, Order>& from_v) BOOST_NOEXCEPT_IF(BOOST_MATH_IS_FLOAT(destination_t) && BOOST_MATH_IS_FLOAT(typename BOOST_IDENTITY_TYPE((differentiation::detail::get_root_type<differentiation::detail::fvar<RealType, Order>>))::type))
+inline BOOST_MATH_CONSTEXPR destination_t real_cast(const differentiation::detail::fvar<RealType, Order>& from_v) BOOST_NOEXCEPT_IF(BOOST_MATH_IS_FLOAT(destination_t) && BOOST_MATH_IS_FLOAT(typename BOOST_IDENTITY_TYPE((differentiation::detail::get_root_type<differentiation::detail::fvar<RealType, Order>>))::type))
 {
   using root_type = typename differentiation::detail::get_root_type<differentiation::detail::fvar<RealType, Order>>::type;
   return static_cast<destination_t>(static_cast<root_type>(from_v));

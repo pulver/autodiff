9a7715522234197c3b926dcc5dc712a6dd368239
Matt Pulver <pulver@users.noreply.github.com>
2019-05-28 16:21:15 -0400

    Update codecov.sh 7a342ed

--- tools/codecov.sh
+++ tools/codecov.sh
@@ -24,27 +24,37 @@ if [ -z "$GCOV" ]; then
     fi
 fi
 
+# lcov after 1.14 needs this
+# sudo apt install --no-install-recommends -y libperlio-gzip-perl libjson-perl
+
+# install the latest lcov we know works
+# some older .travis.yml files install the tip which may be unstable
+rm -rf /tmp/lcov
+pushd /tmp
+git clone -b v1.14 https://github.com/linux-test-project/lcov.git
+export PATH=/tmp/lcov/bin:$PATH
+which lcov
+lcov --version
+popd
+
 B2_VARIANT=debug
 ci/travis/build.sh cxxflags=-fprofile-arcs cxxflags=-ftest-coverage linkflags=-fprofile-arcs linkflags=-ftest-coverage
 
 # switch back to the original source code directory
 cd $TRAVIS_BUILD_DIR
 
-# get the version of lcov
-lcov --version
-
 # coverage files are in ../../b2 from this location
-lcov --gcov-tool=$GCOV --base-directory "$BOOST_ROOT/libs/$SELF" --directory "$BOOST_ROOT" --capture --output-file all.info
+lcov --gcov-tool=$GCOV --rc lcov_branch_coverage=1 --base-directory "$BOOST_ROOT/libs/$SELF" --directory "$BOOST_ROOT" --capture --output-file all.info
 
 # all.info contains all the coverage info for all projects - limit to ours
 # first we extract the interesting headers for our project then we use that list to extract the right things
 for f in `for f in include/boost/*; do echo $f; done | cut -f2- -d/`; do echo "*/$f*"; done > /tmp/interesting
 echo headers that matter:
 cat /tmp/interesting
-xargs -L 999999 -a /tmp/interesting lcov --gcov-tool=$GCOV --extract all.info {} "*/libs/$SELF/src/*" --output-file coverage.info
+xargs -L 999999 -a /tmp/interesting lcov --gcov-tool=$GCOV --rc lcov_branch_coverage=1 --extract all.info {} "*/libs/$SELF/src/*" --output-file coverage.info
 
 # dump a summary on the console - helps us identify problems in pathing
-lcov --gcov-tool=$GCOV --list coverage.info
+lcov --gcov-tool=$GCOV --rc lcov_branch_coverage=1 --list coverage.info
 
 #
 # upload to codecov.io
